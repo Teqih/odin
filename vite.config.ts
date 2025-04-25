a30b83bd41292191e562b46466f39680a2543ee1
stephen foerster <stephen.foerster1@gmail.com>
2025-04-25 16:30:09 +0200

    Fix try

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
@@ -13,7 +12,6 @@ export default defineConfig({
         ]
       }
     }),
-    runtimeErrorOverlay(),
     themePlugin(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
